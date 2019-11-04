d875b1b8e73859774fd1d822a12f9a7319804477
Wilko Dekkers <wilko@mbp-van-wilko.home>
2019-11-04 12:56:08 +0100

    Added: Header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="header-panel">
+            <h1>React Todo App</h1>
+        </div>
+    );
+}
+
+export default Header;
